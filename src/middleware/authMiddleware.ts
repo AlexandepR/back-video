7fcd59aa017e243cb062b91bc0219afb63d8afa3
Alex <alexandepp@gmail.com>
2022-06-23 14:43:30 +0300

    authMiddleware_add

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,17 @@
+import {NextFunction, Request, Response} from "express";
+
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const auth = {login: 'admin', password: 'qwerty'};
+
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = Buffer.from(b64auth,'base64').toString().split(' ');
+
+    if (login && password && login === auth.login && password === auth.password) {
+        return next()
+    }
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required')
+}
+
+export default authMiddleware;
